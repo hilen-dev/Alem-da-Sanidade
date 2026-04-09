4c81a27b56ef0e0e29c8ba5de1b101873fb3d7bc
hilen-dev <joneshilen@gmail.com>
2026-04-09 16:05:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {pericias} from "./data/pericias.js";
 document.addEventListener("DOMContentLoaded", () => {
 
   const container = document.getElementById("pericias-container");
@@ -56,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       function atualizar() {
       const base = p.base;
-      const extra = parseint(input.value)||0;
+      const extra = parseInt(input.value)||0;
       const total = base + extra;
  
         half.textContent = Math.floor(total / 2);
@@ -72,57 +73,53 @@ document.addEventListener("DOMContentLoaded", () => {
     container.appendChild(wrapper);
   }
 
-  const pericias = [
-    ["Antropologia",1],["Arqueologia",1],["Arremessar",20],
-    ["Avaliação",5],["Cavalgar",5],["Charme",15],
-    ["Chaveiro",1],["Consertos Elétricos",10],
-    ["Consertos Mecânicos",10],["Contabilidade",5],
-    ["Direito",5],["Dirigir Automóveis",20],
-    ["Disfarce",5],["Eletrônica",1],["Encontrar",25],
-    ["Escalar",20],["Escutar",20],["Furtividade",20],
-    ["História",5],["Intimidação",15],["Lábia",5],
-    ["Medicina",1],["Mergulho",1],["Mundo Natural",10],
-    ["Mythos de Cthulhu",0],["Natação",20],
-    ["Navegação",10],["Nível de Crédito",0],
-    ["Ocultismo",5],["Operar Maquinário Pesado",1],
-    ["Persuasão",10],["Prestidigitação",10],
-    ["Primeiros Socorros",30],["Psicanálise",1],
-    ["Psicologia",10],["Rastrear",10],
-    ["Saltar",20],["Usar Bibliotecas",20],
-    ["Usar Computadores",5],["Treinar Animais",5]
-  ];
-
   pericias.forEach(p => criarPericia(p[0], p[1]));
 
   criarGrupo("Lutar", [
-    { nome:"Briga",base:25},{ nome:"Chicotes",base:5},
-    { nome:"Espadas",base:20},{ nome:"Garrote",base:15},
-    { nome:"Lanças",base:20},{ nome:"Machados",base:15},
-    { nome:"Manguais",base:10},{ nome:"Motosserras",base:10}
+    { nome:"Briga",base:25},
+    { nome:"Chicotes",base:5},
+    { nome:"Espadas",base:20},
+    { nome:"Garrote",base:15},
+    { nome:"Lanças",base:20},
+    { nome:"Machados",base:15},
+    { nome:"Manguais",base:10},
+    { nome:"Motosserras",base:10}
   ]);
 
   criarGrupo("Armas", [
-    { nome:"Pistolas",base:20},{ nome:"Rifles/Espingardas",base:25},
-    { nome:"Submetralhadoras",base:15},{ nome:"Metralhadoras",base:10},
-    { nome:"Lança-Chamas",base:10},{ nome:"Arcos",base:15}
+    { nome:"Pistolas",base:20},
+    { nome:"Rifles/Espingardas",base:25},
+    { nome:"Submetralhadoras",base:15},
+    { nome:"Metralhadoras",base:10},
+    { nome:"Lança-Chamas",base:10},
+    { nome:"Arcos",base:15}
   ]);
 
   criarGrupo("Ciência", [
-    { nome:"Biologia",base:1},{ nome:"Botânica",base:1},
-    { nome:"Ciência Forense",base:1},{ nome:"Criptografia",base:1},
-    { nome:"Engenharia",base:1},{ nome:"Farmácia",base:1},
-    { nome:"Física",base:1},{ nome:"Geologia",base:1},
-    { nome:"Matemática",base:1},{ nome:"Meteorologia",base:1},
-    { nome:"Química",base:1},{ nome:"Zoologia",base:1}
+    { nome:"Biologia",base:1},
+    { nome:"Botânica",base:1},
+    { nome:"Ciência Forense",base:1},
+    { nome:"Criptografia",base:1},
+    { nome:"Engenharia",base:1},
+    { nome:"Farmácia",base:1},
+    { nome:"Física",base:1},
+    { nome:"Geologia",base:1},
+    { nome:"Matemática",base:1},
+    { nome:"Meteorologia",base:1},
+    { nome:"Química",base:1},
+    { nome:"Zoologia",base:1}
   ]);
 
   criarGrupo("Pilotar", [
-    { nome:"Aeronaves",base:1},{ nome:"Navios",base:1}
+    { nome:"Aeronaves",base:1},
+    { nome:"Navios",base:1}
   ]);
 
   criarGrupo("Arte & Ofício", [
-    { nome:"Atuação",base:5},{ nome:"Belas Artes",base:5},
-    { nome:"Falsificação",base:5},{ nome:"Fotografia",base:5}
+    { nome:"Atuação",base:5},
+    { nome:"Belas Artes",base:5},
+    { nome:"Falsificação",base:5},
+    { nome:"Fotografia",base:5}
   ]);
 
   criarGrupo("Línguas", [
